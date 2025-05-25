a62c7f331cf1758e2c1bd08566ee04284f9c81d4
co-den <iamagugbueikenna@gmail.com>
2025-05-25 23:04:53 +0100

    second commit

--- config/db.js
+++ config/db.js
@@ -4,10 +4,7 @@ dotenv.config({ path: "./config.env" });
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB Connected");
     } catch (error) {
         console.error(error);
